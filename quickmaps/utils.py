dc22d7bf1321d63df0c7539470c85ad2c8dfd975
Daniel Barreto <dbarreto@talpor.com>
2012-09-04 12:54:31 -0430

    A new dict extension for LatLng values called `LatLngDict`. will be used to save geolocations.

---
+++ quickmaps/utils.py
@@ -0,0 +1,24 @@
+from django.template.loader import render_to_string
+from django.utils.encoding import StrAndUnicode
+
+
+class LatLngDict(dict, StrAndUnicode):
+    """A two key'd dictionary for latitude and longitude values that
+    knows how to display itself in various formats.
+    """
+    template_name = None
+
+    def __unicode__(self):
+        return self.as_comma_separated()
+
+    def as_comma_separated(self):
+        if not self:
+            return ''
+        return '%s, %s' % (self['longitude'], self['latitude'])
+
+    def as_map(self, template_name=None, context=None):
+        context = (context or {}).update(self)
+        template_name = template_name or self.template_name
+        if template_name is None:
+            return ''
+        return render_to_string(template_name or self.template_name, context)
